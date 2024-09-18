097d1ac259732b0aad8148af94fe455c04b291d4
CarlosLonghi <carloslonghi.cl@gmail.com>
2024-09-18 18:52:06 -0300

    feat: add exceptions to email send

--- src/main/java/com/longhi/email_service/infra/ses/SesEmailSender.java
+++ src/main/java/com/longhi/email_service/infra/ses/SesEmailSender.java
@@ -4,6 +4,7 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.*;
 import com.longhi.email_service.adapters.EmailSenderGateway;
+import com.longhi.email_service.core.exceptions.EmailServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +29,7 @@ public class SesEmailSender implements EmailSenderGateway {
         try {
             this.amazonSimpleEmailService.sendEmail(request);
         } catch (AmazonServiceException exception) {
-            throw new EmailServiceException("Failure while sending email");
+            throw new EmailServiceException("Failure while sending email", exception);
         }
     }
 }
